a6e588cb3f8618f95b9946b29a274312ba94601a
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-06-07 11:09:31 +0530

    Create  ChildComponent1.js

---
+++ src/ChildComponent1.js
@@ -0,0 +1,16 @@
+
+
+import React from 'react';
+
+const ChildComponent1 = ({ selectedOption, onOptionSelect }) => {
+  return (
+    <div>
+      <h2>Child Component 1</h2>
+      <button onClick={() => onOptionSelect('Option 1')} disabled={selectedOption === 'Option 1'}>
+        Option 1
+      </button>
+    </div>
+  );
+};
+
+export default ChildComponent1;
