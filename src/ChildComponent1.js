8c5030df117003140997294fe2b1661ce8313298
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-06-07 11:20:53 +0530

    Update ChildComponent1.js

--- src/ChildComponent1.js
+++ src/ChildComponent1.js
@@ -1,11 +1,10 @@
-
 import React from 'react';
 
-const ChildComponent1 = ({ onSelect }) => {
+const ChildComponent1 = (props) => {
   return (
     <div>
       <h2>Child Component 1</h2>
-      <button onClick={() => onSelect('Option 1')}>Select Option 1</button>
+      <button onClick={() => props.updateOption("Option 1")}>Option 1</button>
     </div>
   );
 };
