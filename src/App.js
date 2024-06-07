d09f1a42e7765738ef559411f532e5b3c8c76c16
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-06-07 11:16:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-
-
 import React, { useState } from 'react';
 import ChildComponent1 from './ChildComponent1';
 import ChildComponent2 from './ChildComponent2';
@@ -14,8 +12,9 @@ const App = () => {
   return (
     <div className="parent">
       <h1>Parent Component</h1>
-      <ChildComponent1 selectedOption={selectedOption} onOptionSelect={handleOptionSelect} />
-      <ChildComponent2 selectedOption={selectedOption} onOptionSelect={handleOptionSelect} />
+      <p>Selected Option: {selectedOption}</p>
+      <ChildComponent1 onSelect={handleOptionSelect} />
+      <ChildComponent2 onSelect={handleOptionSelect} />
     </div>
   );
 };
